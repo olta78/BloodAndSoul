9461255ba370a6557895b5a07412008b1545877b
Oleg <mycollector78@yandex.ru>
2021-07-26 14:11:16 +0600

    изменил
    изменил

---
+++ Lopata/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lopata
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+        const UInt32 WM_CHAR = 0x31;
+        const int VK_RETURN = 0x0d;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 5000;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e, IntPtr HWND_BROADCAST)
+        {
+            SendMessage(HWND_BROADCAST, WM_CHAR, (IntPtr)VK_RETURN, (IntPtr)1);
+        }
+    }
+}
